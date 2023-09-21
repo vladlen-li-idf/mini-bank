0d3be42526868e481efebf6ea8b8df89ddc8fe7c
vladlen-li-idf <vladlen.li@solva.kz>
2023-09-21 14:19:35 +0600

    feat(borrower): changed initial migration

--- migration/src/main/resources/db/changelog/2023/09/21/21-add-borrower-user-tables.sql
+++ migration/src/main/resources/db/changelog/2023/09/21/21-add-borrower-user-tables.sql
@@ -1,18 +1,28 @@
 CREATE SCHEMA IF NOT EXISTS security;
 CREATE SCHEMA IF NOT EXISTS borrower;
 
-CREATE TABLE IF NOT EXISTS security.user_account (
+CREATE TYPE security.status as enum('ACTIVE', 'CLOSED', 'DRAFT');
+CREATE TYPE borrower.status as enum('ACTIVE', 'CLOSED', 'DRAFT');
+
+CREATE TABLE IF NOT EXISTS security.user (
     id int8,
     email VARCHAR(255) NOT NULL UNIQUE,
     login VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    status security.status,
     citizenship VARCHAR(100),
-    role VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS security.role (
+    id int8,
+    code VARCHAR(100),
+    status security.status
 );
 
 CREATE TABLE IF NOT EXISTS borrower.personal_data (
     id int8,
+    status borrower.status,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     birth_date DATE,
@@ -22,9 +32,10 @@ CREATE TABLE IF NOT EXISTS borrower.personal_data (
 
 CREATE TABLE IF NOT EXISTS borrower.borrower (
     id int8,
-    user_account_id int8,
+    status borrower.status,
+    user_id int8,
     personal_data_id int8,
     PRIMARY KEY (id),
-    CONSTRAINT fk_borrower_borrower_user_account FOREIGN KEY (user_account_id) REFERENCES security.user_account(id),
+    CONSTRAINT fk_borrower_borrower_user_account FOREIGN KEY (user_id) REFERENCES security.user(id),
     CONSTRAINT fk_borrower_borrower_personal_data FOREIGN KEY (personal_data_id) REFERENCES borrower.personal_data(id)
 );
